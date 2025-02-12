fd788de05d0e9b813ac511bc63d4336b3e83178a
erin-yl <erinyinling@gmail.com>
2025-02-11 16:58:25 -0800

    Complete exercise two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, times) {
+  let str = '';
+  if (times < 0) {
+    return 'ERROR';
+  }
+  while (times > 0) {
+    str += word;
+    times --;
+  }
+    return str;
+  }
 
 // Do not edit below this line
 module.exports = repeatString;
