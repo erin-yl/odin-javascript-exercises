271829b0caf5410bb9647a754f6d49bafc76bd83
erin-yl <erinyinling@gmail.com>
2025-02-13 11:49:04 -0800

    Complete exercise seven

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(cel) {
+  const fah = (cel - 32) * 5/9;
+  return Math.round(fah * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fah) {
+  const cel = fah * 9/5 + 32;
+  return Math.round(cel * 10) / 10;
 };
 
 // Do not edit below this line
