5a1d5217bb4cc44790eea6c4980d0160e1761379
erin-yl <erinyinling@gmail.com>
2025-02-13 15:47:34 -0800

    Complete exercise nine

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanedString = str.toLowerCase().replace(/[^\w\s]|_/g, '').replaceAll(' ', '');
+  const reversedString = cleanedString.split('').reverse().join('');
+  return reversedString === cleanedString;
 };
 
 // Do not edit below this line
