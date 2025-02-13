eb1a124cc12512c272d80d44a232ea02744c85bc
erin-yl <erinyinling@gmail.com>
2025-02-12 17:48:33 -0800

    Complete exercise five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  if (!Number.isInteger(min) ||
+      !Number.isInteger(max) ||
+      min < 0 ||
+      max < 0) {
+    return 'ERROR';
+  } else if (min > max) {
+    const num = min;
+    min = max;
+    max = num;
+  }
 
-};
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
