f3f2b9b91c99a348fe9d1b5be04667acb31f7cce
erin-yl <erinyinling@gmail.com>
2025-02-13 16:30:50 -0800

    Complete exercise ten

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+  const count = Number(countArg);
 
+  if (count < 0) return 'OOPS';
+
+  const fib = [0, 1];
+  for (i = 2; i <= count; i++) {
+    fib[i] = fib[i-1] + fib[i-2];
+  }
+  return fib[count];
 };
 
 // Do not edit below this line
